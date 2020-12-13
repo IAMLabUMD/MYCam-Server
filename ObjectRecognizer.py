81cabc954b2e6e18c41a0c4d156c394bc608e2fe
Jonggi Hong <jonggi.hong@gmail.com>
2020-12-13 14:52:34 -0500

    HTTP Server is updated.

--- ObjectRecognizer.py
+++ ObjectRecognizer.py
@@ -1,16 +1,16 @@
-"""
+'''
     A teachable object recognizer class with MobileNetV2 and transfer learning
 
     Author: Jonggi Hong
     Date: 12/13/2020
-"""
+'''
 
 import time
 import numpy as np
-import sys
 import os
 import scipy.stats
 from pathlib import Path
+from shutil import copyfile
 
 from keras.layers import Dense, Flatten
 from keras.preprocessing.image import ImageDataGenerator
@@ -20,14 +20,17 @@ from keras.preprocessing import image
 from keras.applications import MobileNetV2
 from keras.applications.mobilenet import preprocess_input
 
+
 class ObjectRecognizer:
     def __init__(self):
         self.debug = False
         self.input_width = 224
         self.input_height = 224
         self.curr_model_dir = 'no model is loaded.'
+        self.model = None
+        self.labels = None
 
-    """ loads the classification model and labels
+    ''' loads the classification model and labels
         
         Arguments:
             - model_dir: the directory with the model and label files
@@ -35,8 +38,12 @@ class ObjectRecognizer:
         Returns:
             - model: keras model instance from the model file
             - labels: list of labels
-    """
+    '''
+
     def loadModelAndLabels(self, model_dir):
+        if self.curr_model_dir == model_dir:
+            return self.model, self.labels
+
         model_path = os.path.join(model_dir, 'model.h5')
         model = load_model(model_path)
 
@@ -48,7 +55,42 @@ class ObjectRecognizer:
 
         return model, labels
 
-    """ trains the object recognition model and saves the model and labels to files
+    ''' saves the current model and labels to file. When the model is not initialized, 
+        use 'org_dir' parameter to load the model and labels to save in the 'save_dir' directory.
+
+            Arguments:
+                - model_dir: the directory to save the model and labels
+                - org_dir: the directory with model and labels. 
+            Returns:
+    '''
+
+    def saveModelAndLabel(self, save_dir, org_dir=None):
+        if self.debug:
+            print('saving the model...', save_dir)
+        Path(save_dir).mkdir(parents=True, exist_ok=True)  # create the directory if it does not exist.
+
+        # when the original files are given, just copy files.
+        if not org_dir is None:
+            if os.path.isdir(org_dir):
+                copyfile(os.path.join(org_dir, 'model.h5'), os.path.join(save_dir, 'model.h5'))
+                copyfile(os.path.join(org_dir, 'labels.txt'), os.path.join(save_dir, 'labels.txt'))
+                self.model, self.labels = self.loadModelAndLabels(org_dir)
+                return
+            else:
+                print('The model to save is not found (no previous model).')
+
+        # save the model to file
+        model_path = os.path.join(save_dir, 'model.h5')
+        self.model.save(model_path)
+
+        # save the labels to file
+        label_path = os.path.join(save_dir, 'labels.txt')
+        f = open(label_path, 'w')
+        for i in range(len(self.labels)):
+            f.write(self.labels[i] + '\n')
+        f.close()
+
+    ''' trains the object recognition model and saves the model and labels to files
 
             Arguments:
                 - model_dir: the directory to save the model and labels
@@ -56,10 +98,11 @@ class ObjectRecognizer:
 
             Returns:
             
-    """
+    '''
+
     def train(self, model_dir, img_dir):
         if self.debug:
-            print("Start training ...", img_dir)
+            print('Start training ...', img_dir)
 
         start_time = time.time()
         train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)  # included in our dependencies
@@ -76,7 +119,8 @@ class ObjectRecognizer:
             print('training data are collected: ', time.time() - start_time)
 
         # imports the mobilenet model and discards the last 1000 neuron layer.
-        base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(self.input_width, self.input_height, 3))
+        base_model = MobileNetV2(weights='imagenet', include_top=False,
+                                 input_shape=(self.input_width, self.input_height, 3))
 
         if self.debug:
             print('loading the base model (', base_model.name, '): ', time.time() - start_time)
@@ -94,33 +138,24 @@ class ObjectRecognizer:
             print('setting up the training: ', time.time() - start_time)
 
         step_size_train = train_generator.n // train_generator.batch_size
-        model.fit(train_generator, steps_per_epoch=step_size_train, epochs=100)  # 200? # 80: around 2 minutes, 200: around 5 minutes
+        model.fit(train_generator, steps_per_epoch=step_size_train,
+                  epochs=100)  # 200? # 80: around 2 minutes, 200: around 5 minutes, 100: current
 
         if self.debug:
             print('training is done: ', time.time() - start_time)
-
-        Path(model_dir).mkdir(parents=True, exist_ok=True)
-        model_path = os.path.join(model_dir, 'model.h5')
-        label_path = os.path.join(model_dir, 'labels.txt')
-
-        # save the model to file
-        model.save(model_path)
-
-        # save the labels to file
-        f = open(label_path, 'w')
-        for i in range(len(labels2)):
-            f.write(labels2[i] + '\n')
-        f.close()
 
         # set current model and labels
         self.curr_model_dir = model_dir
         self.labels = labels2
         self.model = model
 
+        # save the trained model and labels
+        self.saveModelAndLabel(model_dir)
+
         if self.debug:
             print('saving the model to a file: ', time.time() - start_time)
 
-    """ predicts the object in an image
+    ''' predicts the object in an image
 
             Arguments:
                 - model_dir: the directory with the model and labels
@@ -130,8 +165,13 @@ class ObjectRecognizer:
                 - best_label: the label with the highest confidence score
                 - entropy: entropy of the confidence scores
                 - conf: a dictionary with confidence scores of all labels (label, confidence score)
-    """
+    '''
+
     def predict(self, model_dir, img_path):
+        # if the model does not exist, return None
+        if not os.path.isdir(model_dir):
+            return None, None, None
+
         if self.curr_model_dir != model_dir:
             self.model, self.labels = self.loadModelAndLabels(model_dir)
             self.curr_model_dir = model_dir
@@ -141,7 +181,7 @@ class ObjectRecognizer:
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
 
-        preds = self.model.predict(x)[0]
+        preds = self.model.predict(x)[0].tolist()
         if self.debug:
             print(preds)
 
@@ -156,8 +196,9 @@ class ObjectRecognizer:
 
         return best_label, entropy, conf
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     orec = ObjectRecognizer()
     orec.debug = True
-    # orec.train('model', '/Users/jonggihong/Downloads/tmpImages')
-    orec.test('model', '/Users/jonggihong/Downloads/tmpImages/Knife/1.jpg')
+    orec.train('model', '/Users/jonggihong/Downloads/tmpImages')
+    # orec.predict('model', '/Users/jonggihong/Downloads/tmpImages/Knife/1.jpg')
--- TOR_HTTP_Server_v3.py
+++ TOR_HTTP_Server_v3.py
@@ -4,64 +4,13 @@ from __future__ import print_function
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
-from shutil import copyfile
 
-import argparse
-import scipy.stats
 import sys
-import os
 import traceback
 
 import time
-import csv
-import pandas as pd
-import numpy as np
 import os
-import keras
-import matplotlib.pyplot as plt
-from keras.layers import Dense, GlobalAveragePooling2D, Flatten
-from keras.applications import MobileNet
-from keras.preprocessing import image
-
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Model
-from keras.optimizers import Adam
-from keras.models import load_model
-from keras import backend as K
-from sklearn.metrics import f1_score
-from numpy import asarray
-from numpy import savetxt
-from pathlib import Path
-
 from ObjectRecognizer import ObjectRecognizer
-
-#     from keras.applications.inceptionresnetv2 import preprocess_input
-#     from keras.applications.inceptionresnetv2 import decode_predictions
-# from keras.applications.inception_v3 import InceptionV3
-# from keras.applications.inception_v3 import preprocess_input
-from keras.applications import MobileNet
-from keras.applications.mobilenet import preprocess_input
-
-prevTestID = ""
-model_file = ""
-label_file = ""
-input_height = 299
-input_width = 299
-input_mean = 128
-input_std = 128
-# input_layer = "input"
-input_layer = "Mul"
-output_layer = "final_result"
-
-model = None
-
-input_name = ""
-output_name = ""
-input_operation = None
-output_operation = None
-isTraining = False
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # second gpu
 
 
 # ssh jhong12@128.8.235.4
@@ -76,6 +25,11 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"  # second gpu
 # ssh -N -f -L localhost:4000:localhost:4000 jhong12@128.8.235.4
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self):
+        self.isTraining = False
+        self.object_recognizer = ObjectRecognizer()
+        self.object_recognizer.debug = True
+
     def safeGetValue(self, dic, k):
         if k in dic:
             return dic[k]
@@ -86,11 +40,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         html_body = self.rfile.read(content_length)
 
-        postStr = html_body.decode().replace("%2F", "/")
-        postStr = postStr.split("&")
+        postStr = html_body.decode().replace('%2F', '/')
+        postStr = postStr.split('&')
         params = {}
         for pstr in postStr:
-            pstr = pstr.split("=")
+            pstr = pstr.split('=')
             params[pstr[0]] = pstr[1]
 
         print(params)
@@ -100,224 +54,75 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         img_path = self.safeGetValue(params, 'imgPath')
         return userID, cmd, category, img_path
 
-    def recognize(self, imgPath, userID):
-        print(prevTestID, "....")
-        # 		imgPath = params["imgPath"]
-
-        if self.prevTestID != userID:
-            print("different id")
-            model_file = "/home/jhong12/TOR-app-files/models/" + userID + "/model_" + category + ".h5"
-            label_file = "/home/jhong12/TOR-app-files/models/" + userID + "/label_" + category + ".txt"
-
-            if not os.path.isfile(model_file):
-                output = "Error. Model does not exist."
-                response.write(str.encode(output))
-
-            model = load_model(model_file)
-        else:
-            print("same id")
-
-        img = image.load_img(imgPath, target_size=(224, 224))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
-
-        preds = model.predict(x)[0]
-        print(preds)
-
-        labels = []
-        with open(label_file) as openfileobject:
-            for line in openfileobject:
-                labels.append(line.strip())
-
-        entropy = scipy.stats.entropy(preds)
-        output = "" + str(entropy)
-        for i in range(len(labels)):
-            output = output + "-:-" + str(labels[i]) + "/" + str(preds[i])
-
-        print(output)
-        response.write(str.encode(output))
-
     def do_GET(self):
-        print("GET")
+        print('GET')
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'Hello, world!')
 
     def do_POST(self):
         try:
-            global prevTestID, model_file, label_file, input_height, input_width, input_mean, input_std, input_layer, output_layer, model, input_name, output_name, input_operation, output_operation, isTraining
             start_time = time.time()
-            # 			content_length = int(self.headers['Content-Length'])
-            # 			body = self.rfile.read(content_length)
-            #
-            # 			postStr = body.decode().replace("%2F", "/")
-            # 			postStr = postStr.split("&")
-            # 			params = {}
-            # 			for pstr in postStr:
-            # 				pstr = pstr.split("=")
-            # 				params[pstr[0]] = pstr[1]
-            #
-            #
-            # 			print(params)
-            # 			userID = params["userId"]
-            # 			type = params["type"]
-            # 			category = params["category"]
             userID, cmd, category, img_path = self.parseParams()
+            model_dir = '/home/jhong12/TOR-app-files/models/' + userID
             response = BytesIO()
 
-            if cmd == "test":
-                print(prevTestID, "....")
-                imgPath = params["imgPath"]
+            if cmd == 'test':
+                best_label, entropy, conf = self.object_recognizer.predict(model_dir, img_path)
 
-                if prevTestID != userID:
-                    print("different id")
-                    model_file = "/home/jhong12/TOR-app-files/models/" + userID + "/model_" + category + ".h5"
-                    label_file = "/home/jhong12/TOR-app-files/models/" + userID + "/label_" + category + ".txt"
-
-                    if not os.path.isfile(model_file):
-                        output = "Error. Model does not exist."
-                        response.write(str.encode(output))
-
-                    model = load_model(model_file)
+                output = ''
+                if best_label is None:
+                    output = 'Object recognition model does not exist.'
                 else:
-                    print("same id")
+                    output = str(entropy)
+                    for label, confidence in conf.items():
+                        output = output + '-:-' + label + '/' + str(confidence)
 
-                img = image.load_img(imgPath, target_size=(224, 224))
-                x = image.img_to_array(img)
-                x = np.expand_dims(x, axis=0)
-                x = preprocess_input(x)
-
-                preds = model.predict(x)[0]
-                print(preds)
-
-                labels = []
-                with open(label_file) as openfileobject:
-                    for line in openfileobject:
-                        labels.append(line.strip())
-
-                entropy = scipy.stats.entropy(preds)
-                output = "" + str(entropy)
-                for i in range(len(labels)):
-                    output = output + "-:-" + str(labels[i]) + "/" + str(preds[i])
-
-                print(output)
                 response.write(str.encode(output))
 
-            elif cmd == "loadModel":
-                model_file = "/home/jhong12/TOR-app-files/models/" + userID + "/model_" + category + ".h5"
-                label_file = "/home/jhong12/TOR-app-files/models/" + userID + "/label_" + category + ".txt"
+            elif cmd == 'loadModel':
+                self.object_recognizer.loadModelAndLabels(model_dir)
 
-                model = load_model(model_file)
-
-            elif cmd == "trainRequest":
-                markFile = "/home/jhong12/TOR-app-files/isTraining"
-                f = open(markFile, "w")
-                f.write("yes")
+            elif cmd == 'trainRequest':
+                markFile = '/home/jhong12/TOR-app-files/isTraining'
+                f = open(markFile, 'w')
+                f.write('yes')
                 f.close()
+                train_img_dir = '/home/jhong12/TOR-app-files/photo/TrainFiles/' + userID + '/' + category
 
-                Path("/home/jhong12/TOR-app-files/models/" + userID).mkdir(parents=True, exist_ok=True)
-                os.chmod("/home/jhong12/TOR-app-files/models/" + userID, 0o777)
+                self.object_recognizer.saveModelAndLabel(model_dir+'_prev', org_dir=model_dir)
+                self.object_recognizer.train(model_dir, train_img_dir)
 
-                model_file = "/home/jhong12/TOR-app-files/models/" + userID + "/model_" + category + ".h5"
-                label_file = "/home/jhong12/TOR-app-files/models/" + userID + "/label_" + category + ".txt"
-                model_prev_file = "/home/jhong12/TOR-app-files/models/" + userID + "/model_" + category + "_prev.h5"
-                label_prev_file = "/home/jhong12/TOR-app-files/models/" + userID + "/label_" + category + "_prev.txt"
-
-                if os.path.isfile(model_file):
-                    copyfile(model_file,
-                             "/home/jhong12/TOR-app-files/models/" + userID + "/model_" + category + "_prev.h5")
-                if os.path.isfile(label_file):
-                    copyfile(label_file,
-                             "/home/jhong12/TOR-app-files/models/" + userID + "/label_" + category + "_prev.txt")
-
-                print('preprocessing time: ', time.time() - start_time)
-
-                train_datagen = ImageDataGenerator(
-                    preprocessing_function=preprocess_input)  # included in our dependencies
-                train_path = "/home/jhong12/TOR-app-files/photo/TrainFiles/" + userID + "/" + category
-                print('dir:', train_path)
-                train_generator = train_datagen.flow_from_directory(train_path,
-                                                                    target_size=(224, 224),
-                                                                    color_mode='rgb',
-                                                                    batch_size=30,
-                                                                    class_mode='categorical',
-                                                                    shuffle=True)
-
-                labels = (train_generator.class_indices)
-                labels2 = dict((v, k) for k, v in labels.items())
-                print('collecting training data time: ', time.time() - start_time)
-
-                base_model = MobileNet(weights='imagenet', include_top=False, input_shape=(
-                224, 224, 3))  # imports the mobilenet model and discards the last 1000 neuron layer.
-
-                print('loading a model time: ', time.time() - start_time)
-                x = base_model.output
-                x = Flatten()(x)
-                preds = Dense(len(labels2), activation='softmax')(x)  # final layer with softmax activation
-
-                model = Model(inputs=base_model.input, outputs=preds)
-                model.layers[-1].trainable = True
-
-                model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-                print('setting up the training time: ', time.time() - start_time)
-
-                step_size_train = train_generator.n // train_generator.batch_size
-                model.fit_generator(generator=train_generator,
-                                    steps_per_epoch=step_size_train,
-                                    epochs=100)  # 200? # 80: around 2 minutes, 200: around 5 minutes
-
-                print('training done time: ', time.time() - start_time)
-                model.save(model_file)
-
-                print('save model time: ', time.time() - start_time)
-                # write labels
-
-                f = open(label_file, "w")
-                for i in range(len(labels2)):
-                    f.write(labels2[i] + "\n")
-                f.close()
-                os.chmod(label_file, 0o777)
-                os.chmod(model_file, 0o777)
-                os.chmod(label_prev_file, 0o777)
-                os.chmod(model_prev_file, 0o777)
-
-                f = open(markFile, "w")
-                f.write("no")
+                f = open(markFile, 'w')
+                f.write('no')
                 f.close()
 
                 print('print response: training is done.')
                 response.write(b'Training is done')
-            else:
-                print("Debugging...")
 
-            # 		self.send_response(200)
-            # 		self.end_headers()
-            # 		response.write(b'This is POST request. ')
-            # 		response.write(b'Received: ')
-            # 		response.write(body)
+            else:
+                print('Debugging...')
+
             self.wfile.write(response.getvalue())
         except:
-            print("Exception. Reset training.")
-            markFile = "/home/jhong12/TOR-app-files/isTraining"
-            f = open(markFile, "w")
-            f.write("no")
+            print('Exception. Reset training.')
+            markFile = '/home/jhong12/TOR-app-files/isTraining'
+            f = open(markFile, 'w')
+            f.write('no')
             f.close()
 
             print(traceback.format_exc())
 
             e = sys.exc_info()[0]
-            msg = "Error: %s" % e
+            msg = 'Error: %s' % e
             self.wfile.write(msg.encode())
 
-        prevTestID = userID
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'  # second gpu
 
-
-print("run")
-markFile = "/home/jhong12/TOR-app-files/isTraining"
-f = open(markFile, "w")
-f.write("no")
+print('run')
+markFile = '/home/jhong12/TOR-app-files/isTraining'
+f = open(markFile, 'w')
+f.write('no')
 f.close()
 
 httpd = HTTPServer(('128.8.235.4', 8000), SimpleHTTPRequestHandler)
